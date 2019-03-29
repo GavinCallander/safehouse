0e78f5a9fff8323d5fcec7822ce5311eecb88850
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-03-29 13:45:05 -0700

    links added to images

--- src/MapBox.jsx
+++ src/MapBox.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import ReactMapBoxGl, { Marker } from 'react-mapbox-gl';
+import Binoculars from './images/binoculars-solid.svg';
+import { Link } from 'react-router-dom';
 
 
 class MapBox extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            lat: '47.6076665',
             lon: '-122.33543778148014',
-            lat: '47.6076665'
+            safehouses: [
+            {
+                lat: '47.6205',
+                lon: '-122.3493'
+            }
+        ]
         }
     }
     render() {
@@ -15,6 +23,23 @@ class MapBox extends Component {
             accessToken:
                 'pk.eyJ1IjoiZ2F2aW5jYWxsYW5kZXIiLCJhIjoiY2p0dDlzaHppMTl2OTRlbGxwZzZsZWFxMyJ9.S6ad33axlftFEFULwQsUjw',
             center: [this.state.lon, this.state.lat]
+        })
+        const safehouses = this.state.safehouses.map((house, index) => {
+            return <Marker key={index}
+            coordinates = {[house.lon, house.lat]}
+            anchor = 'bottom'>
+            <a href='./chat'>
+            <img 
+
+                alt = 'marker'
+                src = {
+                    Binoculars
+                }
+                width = '50px'
+                height = '50px'
+                />
+                </a>
+        </Marker>
         })
         return(
             <>
@@ -29,13 +54,12 @@ class MapBox extends Component {
                         anchor = 'bottom'>
                         <img 
                             alt = 'marker'
-                            src = {
-                                'https://www.shareicon.net/download/2015/12/05/682942_map.svg'
-                            }
-                            width = '42px'
-                            height = '42px'
+                            src = {Binoculars}
+                            width = '50px'
+                            height = '50px'
                             />
                     </Marker>
+                    {safehouses}
                 </Map>
             </>
         )
